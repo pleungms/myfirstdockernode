845eb2279b04f489854ca2e4dbf0f4324055afb5
Peter Leung <peleun@microsoft.com>
2018-08-20 23:40:37 +0800

    New

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World! v15');
+  res.send('Hello World! on port 80 v30');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
 });
